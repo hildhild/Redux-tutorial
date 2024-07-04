7d4203e97e938ad0cd972aa431f086ca2df88fb1
hildhild <huy.ledinh@hcmut.edu.vn>
2024-07-04 16:52:11 +0700

    add edit button and fix header padding

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header({page}) {
     const navigate = useNavigate();
 
     return ( 
-        <div className="px-[20px] py-[30px] flex justify-between">
+        <div className="px-[30px] py-[30px] flex justify-between">
             <div className="text-[30px] font-semibold">{page}</div>
             <div className='flex items-center'>
                 <div className='w-[40px] h-[40px] rounded-full border-solid border-[1px] grid place-content-center cursor-pointer mr-[10px] hover:bg-black hover:text-white'>
@@ -19,11 +19,11 @@ function Header({page}) {
                 {
                     page === "Profile"
                     ?
-                    <button onClick={() => {navigate('/profile')}} className='w-[45px] h-[45px] rounded-full border-solid border-[1px] grid place-content-center cursor-pointer mr-[10px] bg-black text-white'>
+                    <button onClick={() => {navigate('/profile')}} className='w-[45px] h-[45px] rounded-full border-solid border-[1px] grid place-content-center cursor-pointer bg-black text-white'>
                         <FontAwesomeIcon icon={faUser}/>
                     </button>
                     :
-                    <button onClick={() => {navigate('/profile')}} className='w-[45px] h-[45px] rounded-full border-solid border-[1px] grid place-content-center cursor-pointer mr-[10px] hover:bg-black hover:text-white'>
+                    <button onClick={() => {navigate('/profile')}} className='w-[45px] h-[45px] rounded-full border-solid border-[1px] grid place-content-center cursor-pointer hover:bg-black hover:text-white'>
                         <FontAwesomeIcon icon={faUser}/>
                     </button>
                 }
--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
+
 
 function Info({ data }) {
+    const navigate = useNavigate();
+
     return (
         <div className="px-[15px]">
+            <div className='flex justify-end mb-[30px]'>
+                <button className='bg-[#EFEFEF] hover:bg-[#D3EAF1] font-semibold rounded-md w-[100px] px-[10px] py-[5px]' onClick={()=>{navigate('/')}}>Edit</button>
+            </div>
             <p className='mb-[30px]'><strong>Name:</strong> {data.name}</p>
             <p className='mb-[30px]'><strong>Age:</strong> {data.age}</p>
             <p className='mb-[30px]'><strong>Phone:</strong> {data.phone}</p>
